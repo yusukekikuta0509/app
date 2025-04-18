e754776b4919629a0e5ccdceaed8dd8985b1b5c4
yusukekikuta0509 <yusukekikuta.05@gmail.com>
2025-04-18 16:22:40 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
   server: {
-    port: 3000,
+    host: '0.0.0.0',   // 全インターフェイスをリッスン
+    port: 3000,        // 任意のポートに変更
+    strictPort: true,  // ポート使用中ならエラー停止
   },
-});
+  plugins: [react()]
+})
